995022f7367bf963167b6f0a42b27c800e4c9d90
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-10-17 11:32:46 -0300

    implement validation with zod at new book form component and refactoring books page

--- src/pages/Books/components/NewBookForm.tsx
+++ src/pages/Books/components/NewBookForm.tsx
@@ -1,11 +1,25 @@
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import z from 'zod'
 
 import * as Dialog from '@radix-ui/react-dialog'
 import { Button } from '../../../components/Button'
 import { TextInput } from '../../../components/TextInput'
 
+const newBookFormSchemaValidation = z.object({
+  codeBook: z.string(),
+  name: z.string(),
+  imageUrl: z.string(),
+  countPage: z.number(),
+  description: z.string(),
+})
+
+type newBookFormType = z.infer<typeof newBookFormSchemaValidation>
+
 export function NewBookForm() {
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit } = useForm<newBookFormType>({
+    resolver: zodResolver(newBookFormSchemaValidation),
+  })
 
   function handleNewBookForm(data: any) {
     console.log(data)
@@ -14,7 +28,7 @@ export function NewBookForm() {
   return (
     <Dialog.Portal>
       <Dialog.Overlay className="bg-black/60 inset-0 fixed" />
-      <Dialog.Content className="fixed bg-[#2A2634] py-8 px-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
+      <Dialog.Content className="fixed bg-gray-600 py-8 px-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-lg w-[480px] shadow-lg shadow-black/25">
         <Dialog.Close>X</Dialog.Close>
         <Dialog.Title>Cadastrar livro</Dialog.Title>
 
--- src/pages/Books/index.tsx
+++ src/pages/Books/index.tsx
@@ -1,9 +1,10 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { Button } from '../../components/Button'
 import { TextInput } from '../../components/TextInput'
+import { BooksTable } from '../Home/components/BooksTable'
 import { NewBookForm } from './components/NewBookForm'
 
-import { Plus, MagnifyingGlass, Trash } from 'phosphor-react'
+import { Plus, MagnifyingGlass } from 'phosphor-react'
 
 export function Books() {
   return (
@@ -36,68 +37,7 @@ export function Books() {
         <div className="bg-gray-700 p-6 rounded-md">
           <h1 className="text-2xl font-bold">Gerenciar Livros</h1>
 
-          <table className="w-full border-collapse min-w-[600px] mt-6 text-sm">
-            <thead className="table w-full table-fixed">
-              <tr className="table w-full table-fixed text-left">
-                <th className="p-4 bg-gray-500 rounded-tl-md">Código</th>
-                <th className="p-4 bg-gray-500  w-1/2">Nome</th>
-                <th className="p-4 bg-gray-500 text-center">Data de criação</th>
-                <th className="p-4 text-center bg-gray-500 rounded-tr-md">
-                  Ações
-                </th>
-              </tr>
-            </thead>
-            <tbody className="table w-full table-fixed">
-              <tr className="table w-full table-fixed">
-                <td className="p-3 border-t-4 border-gray-700 bg-gray-600">
-                  h12343T
-                </td>
-                <td className="p-3 border-t-4 border-gray-700 bg-gray-600 w-1/2">
-                  Do mil ao milhão: Sem cortar o cafezinho
-                </td>
-                <td className="p-3 border-t-4 border-gray-700 bg-gray-600 text-center">
-                  16/10/22
-                </td>
-                <td className="p-3 border-t-4 border-gray-700 bg-gray-600 ">
-                  <div className="flex items-center justify-center gap-2">
-                    <Button.Root>
-                      <Button.Title>Detalhes</Button.Title>
-                    </Button.Root>
-
-                    <Button.Root>
-                      <Button.Icon>
-                        <Trash />
-                      </Button.Icon>
-                    </Button.Root>
-                  </div>
-                </td>
-              </tr>
-              <tr className="table w-full table-fixed">
-                <td className="p-3 border-t-4 border-gray-700 bg-gray-600">
-                  h12343T
-                </td>
-                <td className="p-3 border-t-4 border-gray-700 bg-gray-600 w-1/2">
-                  Do mil ao milhão: Sem cortar o cafezinho
-                </td>
-                <td className="p-3 border-t-4 border-gray-700 bg-gray-600 text-center">
-                  16/10/22
-                </td>
-                <td className="p-3 border-t-4 border-gray-700 bg-gray-600 ">
-                  <div className="flex items-center justify-center gap-2">
-                    <Button.Root>
-                      <Button.Title>Detalhes</Button.Title>
-                    </Button.Root>
-
-                    <Button.Root>
-                      <Button.Icon>
-                        <Trash />
-                      </Button.Icon>
-                    </Button.Root>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <BooksTable />
         </div>
       </section>
     </main>
