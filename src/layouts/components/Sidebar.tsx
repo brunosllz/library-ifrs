6e58effd0fcc77129862b84617054c0b2536909a
Bruno Luiz <brunoslvrlz13@gmail.com>
2022-10-17 12:38:17 -0300

    Refactoring sibe bar component

--- src/layouts/components/Sidebar.tsx
+++ src/layouts/components/Sidebar.tsx
@@ -5,7 +5,7 @@ import { Logo } from '../../assets/Logo'
 
 export function Sidebar() {
   return (
-    <aside className="flex min-h-screen w-full max-w-[240px] flex-col justify-between bg-gray-800 px-4 py-6">
+    <aside className="flex min-h-screen w-full max-w-[240px] flex-col justify-between bg-gray-800 py-6">
       <div className="flex flex-col">
         <header className="flex gap-3 flex-col items-center rounded-md bg-transparent py-6">
           <Logo />
@@ -17,9 +17,9 @@ export function Sidebar() {
             to="/home"
             className={({ isActive }) => {
               return clsx(
-                'flex items-center rounded-md px-4 py-2 gap-2 hover:bg-gray-400 hover:text-gray-800 transition-colors',
+                'flex items-center px-4 py-2 gap-2 hover:bg-gray-700 transition-colors',
                 {
-                  'bg-gray-100 text-gray-700': isActive,
+                  'bg-gray-700 border-r-8 border-violet-900': isActive,
                 },
               )
             }}
@@ -30,31 +30,29 @@ export function Sidebar() {
           </NavLink>
 
           <details className="group">
-            <summary className="flex gap-2 cursor-pointer items-center rounded-lg px-4 py-2">
+            <summary className="flex gap-2 cursor-pointer items-center px-4 py-2 group-open:bg-gray-600">
               <Books size={24} className="" />
 
-              <span className="text-sm font-medium ">Livros</span>
+              <span className="text-sm font-medium">Livros</span>
 
               <span className="ml-auto shrink-0 transition duration-300 group-open:rotate-90">
                 <CaretRight />
               </span>
             </summary>
 
-            <nav aria-label="Books Nav" className="mt-1.5 ml-4 flex flex-col">
+            <nav aria-label="Books Nav" className="flex flex-col">
               <NavLink
                 to="books"
                 className={({ isActive }) => {
                   return clsx(
-                    'flex items-center rounded-md px-4 py-2 gap-2 hover:bg-gray-400 hover:text-gray-800 transition-colors',
+                    'flex items-center px-4 py-2 gap-2 hover:bg-gray-700 transition-colors pl-8',
                     {
-                      'bg-gray-100 text-gray-700': isActive,
+                      'bg-gray-700 border-r-8 border-violet-900': isActive,
                     },
                   )
                 }}
               >
-                <span className="ml-3 text-sm font-medium">
-                  Gerenciar livros
-                </span>
+                <span className="text-sm font-medium">Gerenciar livros</span>
               </NavLink>
             </nav>
           </details>
